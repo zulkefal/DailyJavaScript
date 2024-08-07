9c20b83466f2ca0e593082242c3c43b8a72555ed
zulkefal <zulkefal.khan705@gmail.com>
2024-08-07 23:46:12 +0500

    day1

---
+++ Variables.js
@@ -0,0 +1,25 @@
+//Declare Using Var
+
+var variable = 10;
+// console.log(variable)
+
+let vars = 20;
+console.log(vars);
+
+let bool = true;
+// console.log(Boolean(bool))
+
+const a = 10;
+console.log("OUtside a", a);
+function sample() {
+    const a = 20;
+  console.log("inside a", a);
+}
+sample();
+console.log("ourside a", a);
+
+const o = {
+  name: "Zulkefal",
+  age: 20,
+};
+// console.log(typeof o);
