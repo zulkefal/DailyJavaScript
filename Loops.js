3c94c8240312b016da686b43874eb0e382743f01
zulkefal <zulkefal.khan705@gmail.com>
2024-08-09 16:57:47 +0500

    day4

---
+++ Loops.js
@@ -0,0 +1,85 @@
+const printNum=()=>{
+    for(i=0;i<10;i++)
+        {
+        
+            console.log(`Number is ${i+1}`);
+
+        }
+}
+
+printNum();
+
+const printNum5=()=>{
+    for(i=0;i<10;i++)
+        {
+        
+            console.log(`5 X ${i+1} = ${5*(i+1)}`);
+
+        }
+}
+
+printNum5();
+
+const whileSum= ()=>{
+    let result=0;
+    let i=0;
+    while(i<10)
+    {
+        result+=i;
+        i++;
+    }
+    return result;
+}
+
+const finalResult=whileSum();
+console.log(finalResult)
+
+const whileSum10= ()=>{
+    let result=0;
+    let i=10;
+    while(i>0)
+    {
+        result+=i;
+        i--;
+    }
+    return result;
+}
+
+const finalResults=whileSum10();
+console.log(finalResults)
+
+const doWhileSum=()=>{
+    let result=0;
+    let i=0;
+    do{
+        result+=i;
+        i++;
+    }while(i<5)
+
+    console.log("Result is ",result);
+}
+
+doWhileSum()
+
+const calculateFactorial=(a)=>{
+    let result=1;
+    for(i=1;i<=a;i++)
+    {
+        result*=i;
+    }
+    console.log(`Factorial of ${a} is ${result}`);
+}
+calculateFactorial(5);
+
+const printStars=()=>{
+    for(i=0;i<5;i++)
+    {
+        let pattern=''
+        for(j=0;j<=i;j++)
+        {
+            pattern+='*';
+        }
+        console.log(pattern)
+    }
+}
+printStars();
