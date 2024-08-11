7338fc1cd1f161538a631c96a3f18ff2b75eb674
zulkefal <zulkefal.khan705@gmail.com>
2024-08-11 15:11:01 +0500

    commit

--- Slice_Splice.js
+++ Slice_Splice.js
@@ -1,6 +1,16 @@
 const cities = ["Tokyo","Cairo","Los Angeles","Paris","Seattle"];
+const newCityArrSplice = cities.splice(1,3);
+// slice make changes in the original array
+//start from index 1 and do include till index 3
+console.log("Original: ", cities)
+console.log("New: ", newCityArrSplice)
 
-const newCityArr = cities.splice(1,3);
+
+const citiess = ["Tokyo","Cairo","Los Angeles","Paris","Seattle"];
+//slice dont affect orignal array
+const newCityArr = citiess.slice(1,3);
+//start from index 1 and donot include till index 3
+
 
 console.log("Original: ", cities)
 console.log("New: ", newCityArr)
