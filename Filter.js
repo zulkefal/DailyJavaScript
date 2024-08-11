0435a6bf5ba7b3b04a609593d153295d6c5e0462
zulkefal <zulkefal.khan705@gmail.com>
2024-08-11 14:28:24 +0500

    commit

--- Filter.js
+++ Filter.js
@@ -1,13 +1,34 @@
-const ages = [32, 33, 16, 40];
-const result = ages.filter((age)=>age!==32);
-console.log(result);
-console.log(ages)
+// const ages = [32, 33, 16, 40];
+// const result = ages.filter((age)=>age!==32);
+// console.log(result);
+// console.log(ages)
 
 
 
-const resultAgain = ages.filter((age)=>
+// const resultAgain = ages.filter((age)=>
+// {
+//     return age!==32
+// });
+// console.log(ages);
+// console.log(resultAgain)
+
+
+const objects= [{
+    name:"abc",
+    age:32
+},
 {
-    return age!==32
-});
-console.log(ages);
-console.log(resultAgain)
+    name:"def",
+    age:33
+},
+{
+    name:"ghi",
+    age:16
+},
+{
+    name:"jkl",
+    age:40
+}]
+
+const result = objects.filter((object)=>object.age!==32);
+console.log(result)
