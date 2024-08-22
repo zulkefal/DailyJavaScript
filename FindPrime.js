600cf0c7af9b2ca0b30ff34a1c25edcbe6a00822
zulkefal <zulkefal.khan705@gmail.com>
2024-08-22 19:29:46 +0500

    commit

--- FindPrime.js
+++ FindPrime.js
@@ -1,16 +1,22 @@
 
 const findPrime = () => {
-    for(let i=3;i<10;i++)
+    for(let i=3;i<100;i++)
     {
         if(i%2!==0)
-        {
-            for(let j=2;j<=i;j++)
+        {   let prime= true;
+            for(let j=2;j<i;j++)
                 {
-                    if(j%i==0)
+                    
+                    if(i%j==0)
                     {
-                        console.log(i)
+                        prime= false;
                     }
+
                 }
+            if(prime)
+            {
+                console.log(i);
+            }
         }
        
     }
